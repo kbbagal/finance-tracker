868b69ce82af4d34b89da14bb7b767e394c89e24
= <=>
2020-04-21 23:37:56 +0530

    Introduced IEX interaction client secure API keys storage

---
+++ app/models/stock.rb
@@ -0,0 +1,10 @@
+class Stock < ApplicationRecord
+  def self.lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_publishable_key,
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+
+    client.price(ticker_symbol)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_090352) do
+ActiveRecord::Schema.define(version: 2020_04_21_141934) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.string "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
