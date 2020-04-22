9b9834bf8742e55369fe2e8b28806ddd00b089ca
= <=>
2020-04-22 11:47:00 +0530

    Need to check issue

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,6 +7,7 @@ class Stock < ApplicationRecord
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price:client.price(ticker_symbol))
     rescue Exception => e
+      print e
       return nil
     end
   end
